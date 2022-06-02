8c50f79727d98cd0bffe87ab4f7f8bc3c738bab0
MKAtkinson <maxatkinson@ymail.com>
2022-06-01 19:41:43 -0500

    new files and questions created

---
+++ assets/js/javascript.js
@@ -0,0 +1,46 @@
+var questions = [{
+    id: 0,
+    q: "What is a 'for' loop used for?",
+    a: [{text: "comparing two statements", isCorrect: false},
+    {text: "checking what somthing is for", isCorrect: false},
+    {text: "iterating through an array", isCorrect: true},
+    {text: "one, two, three or for", isCorrect: false}
+    ]
+},
+{   id: 1,
+    q: "Which is NOT a common data type?",
+    a: [{text: "boolean", isCorrect: false},
+    {text: "strings", isCorrect: false},
+    {text: "numbers", isCorrect: false},
+    {text: "alerts", isCorrect: true}
+    ]
+},    
+{   id: 2,
+    q: "The condition in if/else statements are enclosed with?",
+    a: [{text: "curly brackets", isCorrect: false},
+    {text: "square brackets", isCorrect: false},
+    {text: "parentheses", isCorrect: true},
+    {text: "commas", isCorrect: false}
+    ]
+},
+{   id: 3,
+    q: "Arrays in Javascript can store which of these",
+    a: [{text: "numbers and strings", isCorrect: false},
+    {text: "other arryas", isCorrect: false},
+    {text: "booleans", isCorrect: false},
+    {text: "all of the above", isCorrect: true}
+    ]
+},
+{   id: 4,
+    q: "A useful tool for debugging during development and debugging for printing content to the debugger is:",
+    a: [{text: "console.log", isCorrect: true},
+    {text: "for loops", isCorrect: false},
+    {text: "javascript", isCorrect: false},
+    {text: "gitbash/terminal", isCorrect: false}
+    ]
+},
+
+]
+
+
+addEventListener.on("click",)
